b8b804b1b7128b1930db752e8d2daa324bca1702
Ryan Lopopolo <rjl@hyperbo.la>
2011-07-09 07:11:53 -0700

    Repo init with small power hour script

---
+++ README.md
@@ -0,0 +1,3 @@
+I love writing power hour apps.
+
+So far I've done it JS, Actionscript, and twice in Ruby. I have all of those implementations somewhere. I'll find them eventually.
---
+++ power_hour.rb
@@ -0,0 +1,60 @@
+#!/usr/bin/env ruby
+
+require "optparse"
+require "URI"
+
+ITUNES_XML = "$HOME/Music/iTunes/iTunes Music Library.xml"
+
+options = {}
+opt = OptionParser.new do |opts|
+  opts.banner = "Usage: #{$0} [options]\n\n"
+  options[:songs] = 60
+  opts.on("-n", "--number-of-songs NUMBER", Integer,  \
+      "Number of songs in the power hour (default 60)") do |songs|
+    options[:songs] = songs
+  end
+  options[:duration] = 60
+  opts.on("-d", "--duration SECONDS", Integer, \
+      "Duration of each song in seconds (default 60)") do |duration|
+    options[:duration] = duration
+  end
+  options[:dir] = nil
+  opts.on("-D", "--directory DIR", \
+      "Use DIR of music files instead of the iTunes XML") do |dir|
+    options[:dir] = dir
+  end
+  opts.on("-h", "--help", "Display this screen") do
+    puts opts
+    abort
+  end
+end
+opt.parse!
+
+# wrap decode because it's in different locations in
+# 1.8 and 1.9
+def decode string
+  return URI::decode(string) if RUBY_VERSION.include? "1.8"
+  URI::Escape::decode(string)
+end
+
+def get_random_file(list)
+  return decode($1) if /<string>file:\/\/localhost(.+)<\/string>/ =~ \
+    list[rand(list.length)]
+  list[rand(list.length)]
+end
+
+# find all of the paths in source
+# this assumes there are only audio files in the source
+if options[:dir].nil?
+  list_of_files = %x[grep "Location" "#{ITUNES_XML}"].split("\n")
+else
+  puts options[:dir]
+  list_of_files = %x[find "#{options[:dir]}" -type f].split("\n")
+end
+options[:songs].times do |minute|
+  begin
+    candidate = get_random_file(list_of_files)
+    puts "##{minute}: Playing #{candidate}"
+    %x[afplay "#{candidate}" -t #{options[:duration]}]
+  end while $? != 0
+end

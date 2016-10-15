lopopolo/powerhour

commit f01d6a4a8f943fabedab11ad6a9a696600ec9ab2

Convert afplay subprocess from Kernel.open to Process.spawn

pwrhr/pwrhr.rb
@@ -142,31 +142,35 @@ module Powerhour
       }
     end
 
-    # execute the song playing command with the given song
-    def execute_command(candidate)
-      afplay_command = "afplay -t #{@duration} \"#{candidate}\" &> /dev/null"
-      exec(afplay_command)
+    def afplay_command(candidate)
+      %Q[afplay -t #{@duration} "#{candidate}"]
     end
 
     # check the state of the child song-playing process over
     # the course of the minute. This method updates the gui as
     # the minute progresses. It also ensures the child is
     # terminated.
-    def monitor_child_process(child)
+    def monitor_child_process(child_pid)
       begin
         start = Time.now
         child_is_eof = false
-        until (delta = Time.now - start) >= @duration || @terminate || @skip || !@playing
+        until (delta = Time.now - start) >= @duration
           @gui.elapsed_song_time = delta
           @gui.elapsed_session_time = @minute * @duration + delta
           @gui.paint
+
+          if @terminate || @skip || !@playing
+            Process.kill("SIGKILL", child_pid)
+            break
+          end
+
           to = 0.01 * duration
           to = 0.1 if to == 0
           # check if child process has finished
           if !child_is_eof
             begin
               child_is_eof = Timeout.timeout(to) {
-                Process.wait(child.pid)
+                Process.wait(child_pid)
               }
             rescue Timeout::Error
               # do nothing; this is expected, so continue looping
@@ -181,7 +185,6 @@ module Powerhour
           end
         end
       ensure
-        child.close
         Process.exit if @terminate
       end
     end
@@ -197,17 +200,12 @@ module Powerhour
       @gui.paint
 
       # fork to execute the music command
-      open("|-", "r+") do |child|
-        begin
-          if child # this is the parent process
-            monitor_child_process(child)
-          else # in child
-            execute_command(song)
-          end
-        rescue
-          # there was a failure
-          # this is ok, $? will be nonzero
-        end
+      begin
+        child_pid = Process.spawn(afplay_command(song))
+        monitor_child_process(child_pid)
+      rescue
+        # there was a failure
+        # this is ok, $? will be nonzero
       end
 
       if !@playing

commit 0667cb7bc7fd283dfa10f90023999fdf97dca810

Fix bug where song counter never increased and powerhours never ended
The conversion from fork+exec to Process.spawn in f01d6a4a8 caused $? to not
get set anymore. This caused the code to interpret every song as failed and loop
forever.

pwrhr/pwrhr.rb
@@ -56,6 +56,9 @@ module Powerhour
   GETCH_TIMEOUT = 0.1
   MUSIC_FILETYPES = %w[aac m4a mp3 mp4]
 
+  SONG_SUCCESS_CODE = 0
+  SONG_FAILED_CODE = 1
+
   # Parse options into a hash that is also populated with default values
   def self.parse_options
     options = {}
@@ -197,6 +200,11 @@ module Powerhour
       ensure
         Process.exit if @terminate
       end
+      if status.nil?
+        SONG_SUCCESS_CODE
+      else
+        SONG_FAILED_CODE
+      end
     end
 
     # try playing a song for the current minute.
@@ -212,10 +220,10 @@ module Powerhour
       # fork to execute the music command
       begin
         child_pid = Process.spawn(afplay_command(song))
-        monitor_child_process(child_pid)
+        retcode = monitor_child_process(child_pid)
       rescue
-        # there was a failure
-        # this is ok, $? will be nonzero
+        # there was a failure; assign fail code
+        retcode = SONG_FAILED_CODE
       end
 
       unless @playing
@@ -224,6 +232,8 @@ module Powerhour
       if @playlist.empty?
         @playlist = @all_files.shuffle
       end
+
+      retcode
     end
 
     # initialize the thread, which contains the main game loop
@@ -241,8 +251,8 @@ module Powerhour
           # because we want to try a song
           # and stop as soon as one is successful
           begin
-            try_song
-          end while $? != 0 && !@skip && @playing
+            status = try_song
+          end while status != SONG_SUCCESS_CODE && !@skip && @playing
 
           # if we didn't abort because we skipped or paused,
           # the song was successful, so increment the minute
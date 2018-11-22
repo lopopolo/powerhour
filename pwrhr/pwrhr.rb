322b1a86211bdf1ff5132b619bbcc9014b2d2581
Ryan Lopopolo <rjl@hyperbo.la>
2018-11-21 20:36:30 -0800

    Move input polling to UI adapter

--- pwrhr/pwrhr.rb
+++ pwrhr/pwrhr.rb
@@ -27,23 +27,23 @@ module Powerhour
       )
 
       playlist = Source.new(options.source_path).playlist
-      ui = CursesUI.new
+      ui = UI::TextInteractive.new
       player = Player.new(playlist)
       game = Game.new(config, player).start
 
       begin
         ui.init
-        while game.active?
+        shutdown_received = false
+        while game.active? && !shutdown_received
           ui.maybe_paint(game.state)
-
-          case Curses.getch
-          when Curses::Key::RIGHT, 's', 'S'
-            player.advance
-          when 'p', 'P'
-            player.toggle
-          when 'q', 'Q'
-            player.shutdown
-            break
+          ui.poll do |command|
+            case command
+            when UI::Command::SKIP then player.advance
+            when UI::Command::TOGGLE then player.toggle
+            when UI::Command::QUIT
+              player.shutdown
+              shutdown_received = true
+            end
           end
         end
       ensure
@@ -223,129 +223,147 @@ module Powerhour
     end
   end
 
-  class CursesUI
-    GETCH_TIMEOUT = 0.1
-
-    COLOR_BEER_TOP = 1
-    COLOR_BEER_BOTTLE = 2
-    COLOR_BEER_LABEL = 3
-    COLOR_NORMAL = 4
-
-    BEER = [
-      [' [=] ', COLOR_BEER_TOP],
-      [' | | ', COLOR_BEER_BOTTLE],
-      [' }@{ ', COLOR_BEER_LABEL],
-      ['/   \\', COLOR_BEER_BOTTLE],
-      [':___;', COLOR_BEER_BOTTLE],
-      ['|&&&|', COLOR_BEER_LABEL],
-      ['|&&&|', COLOR_BEER_LABEL],
-      ['|---|', COLOR_BEER_BOTTLE],
-      ["'---'", COLOR_BEER_BOTTLE]
-    ].freeze
-
-    def init
-      Curses.init_screen
-      Curses.noecho
-      Curses.stdscr.keypad(true) # enable arrow keys
-      Curses.curs_set(0)
-      Curses.timeout = GETCH_TIMEOUT
-      Curses.start_color
-      Curses.use_default_colors
-      Curses.init_pair(COLOR_BEER_TOP, Curses::COLOR_WHITE, -1)
-      Curses.init_pair(COLOR_BEER_BOTTLE, Curses::COLOR_YELLOW, -1)
-      Curses.init_pair(COLOR_BEER_LABEL, Curses::COLOR_RED, -1)
-      Curses.init_pair(COLOR_NORMAL, -1, -1)
+  module UI
+    module Command
+      SKIP = 'skip'
+      TOGGLE = 'toggle'
+      QUIT = 'quit'
     end
 
-    def close
-      Curses.close_screen
-      Curses.curs_set(1)
-    end
+    class TextInteractive
+      GETCH_TIMEOUT = 0.1
 
-    def maybe_paint(state)
-      return paint(state) if @rows.nil? || @cols.nil?
-      return paint(state) if @state.nil?
-      return paint(state) unless @rows == Curses.lines && @cols == Curses.cols
+      COLOR_BEER_TOP = 1
+      COLOR_BEER_BOTTLE = 2
+      COLOR_BEER_LABEL = 3
+      COLOR_NORMAL = 4
 
-      update_metadata(state.metadata) unless state.metadata == @state.metadata
-      update_cursor(state.config) unless state.config.cursor == @state.config.cursor
-      update_elapsed(state.config) unless state.config.elapsed == @state.config.elapsed
-      nil
-    ensure
-      @state = state
-      Curses.refresh
-    end
+      BEER = [
+        [' [=] ', COLOR_BEER_TOP],
+        [' | | ', COLOR_BEER_BOTTLE],
+        [' }@{ ', COLOR_BEER_LABEL],
+        ['/   \\', COLOR_BEER_BOTTLE],
+        [':___;', COLOR_BEER_BOTTLE],
+        ['|&&&|', COLOR_BEER_LABEL],
+        ['|&&&|', COLOR_BEER_LABEL],
+        ['|---|', COLOR_BEER_BOTTLE],
+        ["'---'", COLOR_BEER_BOTTLE]
+      ].freeze
 
-    private
-
-    def paint(state)
-      @rows = Curses.lines
-      @cols = Curses.cols
-      Curses.clear
-      # Static chrome
-      write_line(0, 'Welcome to pwrhr, serving all of your power hour needs')
-      write_line(2, 'Now Playing:')
-      write_line(@rows - 1, 'Enter q to quit, s to skip song, p to toggle play/pause')
-      beer(5, 3)
-      # Dynamic UI
-      update_metadata(state.metadata)
-      update_cursor(state.config)
-      update_elapsed(state.config)
-      nil
-    ensure
-      Curses.refresh
-    end
-
-    def update_cursor(config)
-      write(1, 0, "Song #{config.cursor + 1} of #{config.iterations}")
-    end
-
-    def update_metadata(metadata)
-      write_line(3, "    #{metadata.title}")
-      write_line(4, "    #{metadata.artist} -- #{metadata.title}")
-    end
-
-    def update_elapsed(config)
-      write_line(@rows - 3, progress(config.position, config.duration))
-      write_line(@rows - 2, progress(config.elapsed, config.game_duration))
-    end
-
-    def beer(top_height, bottom_height)
-      avail_height = @rows - top_height - bottom_height
-      line = (avail_height - BEER.size) / 2 + top_height
-      BEER.each_with_index do |(ascii, color), index|
-        write_col = (@cols - ascii.length) / 2
-        write(line + index, write_col, ascii, color)
+      def init
+        Curses.init_screen
+        Curses.noecho
+        Curses.stdscr.keypad(true) # enable arrow keys
+        Curses.curs_set(0)
+        Curses.timeout = GETCH_TIMEOUT
+        Curses.start_color
+        Curses.use_default_colors
+        Curses.init_pair(COLOR_BEER_TOP, Curses::COLOR_WHITE, -1)
+        Curses.init_pair(COLOR_BEER_BOTTLE, Curses::COLOR_YELLOW, -1)
+        Curses.init_pair(COLOR_BEER_LABEL, Curses::COLOR_RED, -1)
+        Curses.init_pair(COLOR_NORMAL, -1, -1)
       end
-    end
 
-    def write(line, col, text, color = COLOR_NORMAL)
-      Curses.setpos(line, col)
-      Curses.attron(Curses.color_pair(color)) { Curses.addstr(text) }
-    end
+      def close
+        Curses.close_screen
+        Curses.curs_set(1)
+      end
 
-    def write_line(line, text, color = COLOR_NORMAL)
-      clear_line(line)
-      write(line, 0, text, color)
-    end
+      def poll
+        command =
+          case Curses.getch
+          when Curses::Key::RIGHT, 's', 'S' then Command::SKIP
+          when 'p', 'P' then Command::TOGGLE
+          when 'q', 'Q' then Command::QUIT
+          end
+        yield command unless command.nil?
+      end
 
-    def clear_line(line)
-      blanks = ' ' * Curses.cols
-      write(line, 0, blanks)
-    end
+      def maybe_paint(state)
+        return paint(state) if @rows.nil? || @cols.nil?
+        return paint(state) if @state.nil?
+        return paint(state) unless @rows == Curses.lines && @cols == Curses.cols
 
-    def format_duration(seconds)
-      Time.at(seconds).utc.strftime('%H:%M:%S').delete_prefix('00:')
-    end
+        update_metadata(state.metadata) unless state.metadata == @state.metadata
+        update_cursor(state.config) unless state.config.cursor == @state.config.cursor
+        update_elapsed(state.config) unless state.config.elapsed == @state.config.elapsed
+        nil
+      ensure
+        @state = state
+        Curses.refresh
+      end
 
-    def progress(elapsed, duration)
-      percent = 1.0 * elapsed / duration
-      suffix = "[#{format_duration(elapsed)} elapsed / #{format_duration(duration)}]"
-      width = [Curses.cols - suffix.length - 2, 0].max
-      progress_width = [(percent * width).ceil, width].min
-      progress = '=' * progress_width
-      remaining = ' ' * [width - progress_width, 0].max
-      "|#{progress}#{remaining}|#{suffix}"
+      private
+
+      def paint(state)
+        @rows = Curses.lines
+        @cols = Curses.cols
+        Curses.clear
+        # Static chrome
+        write_line(0, 'Welcome to pwrhr, serving all of your power hour needs')
+        write_line(2, 'Now Playing:')
+        write_line(@rows - 1, 'Enter q to quit, s to skip song, p to toggle play/pause')
+        beer(5, 3)
+        # Dynamic UI
+        update_metadata(state.metadata)
+        update_cursor(state.config)
+        update_elapsed(state.config)
+        nil
+      ensure
+        Curses.refresh
+      end
+
+      def update_cursor(config)
+        write(1, 0, "Song #{config.cursor + 1} of #{config.iterations}")
+      end
+
+      def update_metadata(metadata)
+        write_line(3, "    #{metadata.title}")
+        write_line(4, "    #{metadata.artist} -- #{metadata.title}")
+      end
+
+      def update_elapsed(config)
+        write_line(@rows - 3, progress(config.position, config.duration))
+        write_line(@rows - 2, progress(config.elapsed, config.game_duration))
+      end
+
+      def beer(top_height, bottom_height)
+        avail_height = @rows - top_height - bottom_height
+        line = (avail_height - BEER.size) / 2 + top_height
+        BEER.each_with_index do |(ascii, color), index|
+          write_col = (@cols - ascii.length) / 2
+          write(line + index, write_col, ascii, color)
+        end
+      end
+
+      def write(line, col, text, color = COLOR_NORMAL)
+        Curses.setpos(line, col)
+        Curses.attron(Curses.color_pair(color)) { Curses.addstr(text) }
+      end
+
+      def write_line(line, text, color = COLOR_NORMAL)
+        clear_line(line)
+        write(line, 0, text, color)
+      end
+
+      def clear_line(line)
+        blanks = ' ' * Curses.cols
+        write(line, 0, blanks)
+      end
+
+      def format_duration(seconds)
+        Time.at(seconds).utc.strftime('%H:%M:%S').delete_prefix('00:')
+      end
+
+      def progress(elapsed, duration)
+        percent = 1.0 * elapsed / duration
+        suffix = "[#{format_duration(elapsed)} elapsed / #{format_duration(duration)}]"
+        width = [Curses.cols - suffix.length - 2, 0].max
+        progress_width = [(percent * width).ceil, width].min
+        progress = '=' * progress_width
+        remaining = ' ' * [width - progress_width, 0].max
+        "|#{progress}#{remaining}|#{suffix}"
+      end
     end
   end
 end
